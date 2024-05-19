effa754222a0e682a9727ac88c4c0fc58d28c58b
Rodrigo Weber <rodrigo.novais@live.com>
2024-05-18 21:17:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# FoRMULÁRIO BASE
+
+## FUNCIONALIDADE
+Um formulário básico para cadastro de usuarios com integração a banco de dados.
+
+## Tecnologias
+- INICIALMENTE HTML
+- PHP
+- CSS
+- XAMPP
+- MySQL
---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUser = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-abby';
+
+    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    // if ($conexao->connect_error) {
+    //     echo "Erro";
+    // } else {
+    //     echo "Conexao com sucesso";
+    // }
+ ?>
